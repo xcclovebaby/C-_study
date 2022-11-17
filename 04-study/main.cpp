791c59b05b9a22852757bec8626bdbbf4f562ea7
chcc <601553044@qq.com>
2022-11-17 17:26:19 +0800

    study

--- 04-study/main.cpp
+++ 04-study/main.cpp
@@ -13,6 +13,12 @@ using namespace std;
  * 6.this指针
  * 7.const修饰成员函数
  * 8.友元
+ * 9.运算符重载
+ * 10.使用运算符重载和友元重写cout<<
+ * 11.重载自增(++)和自减(--)运算符
+ * 12.智能指针
+ * 13.重载等号(=)
+ * 14.重载等于(==)和不等于(!=)
  *  ==========================
  */
 
@@ -179,8 +185,209 @@ public:
 };
 
 /** 8.友元 */
-/*
+/* 友元函数是一种特权函数,C++允许这种特权函数访问类的私有成员
+ * 可以把一个全局函数/类的成员函数/类 声明成友元
+ * 使用friend关键字来定义友元
  */
+class P8 {
+    //定义友元函数
+    friend void show(P8 &p);
+    //定义友元类
+    friend class Good;
+public:
+    P8(string name) {
+        this->name = name;
+    }
+private:
+    string name;
+};
+//声明一个访问私有变量的函数
+void show(P8 &p) {
+    cout<< p.name<< endl;
+}
+//友元类
+class Good{
+public:
+    Good(string name); //构造器可以放在类外面来声明
+    void visit(); //函数也可以放在类外面声明
+private:
+    P8 *p;
+};
+Good::Good(string name) {
+    this->p = new P8(name);
+}
+//访问P8的私有化成员变量
+void Good::visit() {
+    cout<< this->p->name<< endl;
+}
+void test8() {
+    P8 *p = new P8("cc");
+    show(*p);
+    Good *g = new Good("cc");
+    g->visit();
+}
+
+/** 9.运算符重载 */
+/* 运算符重载就是对已有的运算符重新定义,赋予其另一种功能
+ * operator@来给运算符重新定义, @就代表运算符
+ */
+class P9{
+public:
+    int age;
+    P9(int age) {
+        this->age = age;
+    }
+    void show() {
+        cout<< this->age<< endl;
+    }
+    //方式1:类成员函数的方式给+赋予一个新的功能
+//    P9 operator+(P9 &p2) {
+//        return P9(this->age + p2.age);
+//    }
+};
+//方式2:全局函数的方式给+赋予一个新的功能
+P9 operator+(P9 &p1, P9 &p2) {
+   return P9(p1.age + p2.age);
+}
+void test9() {
+    P9 p1(10), p2(20);
+    P9 p3 = p1 + p2;
+    p3.show();
+}
+
+/** 10.使用运算符重载和友元重写cout<< */
+/* cout类型是ostream类型
+ */
+class P10{
+    //定义友元函数
+    friend ostream & operator<<(ostream &cout, P10 &p);
+public:
+    P10(int age) {
+        this->age = age;
+    }
+private:
+    int age;
+};
+//重载cout<<
+ostream & operator<<(ostream &cout, P10 &p) {
+    cout<< p.age;
+    return cout;
+}
+void test10() {
+    P10 p = P10(40);
+    cout<< p<< endl;
+}
+
+/** 11.重载自增(++)和自减(--)运算符 */
+class P11{
+public:
+    P11(int num) {
+        this->num = num;
+    }
+    int num;
+    //实现++p
+    P11 & operator++() {
+        this->num += 1;
+        return *this;
+    }
+    //实现--p
+    P11 & operator--() {
+        this->num -= 1;
+        return *this;
+    }
+    //实现p++
+    P11 operator++(int) {
+        P11 tmp = *this;
+        this->num = this->num + 1;
+        return tmp; //局部变量随着函数的结束而被释放,所以需要返回一个新的对象
+    }
+    //实现p--
+    P11 operator--(int) {
+        P11 tmp = *this;
+        this->num -= 1;
+        return tmp;
+    }
+};
+ostream & operator<<(ostream &cout, P11 &p) {
+    cout<< p.num;
+    return cout;
+}
+void test11() {
+    P11 p = P11(1);
+    cout<< ++p<< endl;
+    P11 p1 = p++;
+    cout<< p1<< endl;
+}
+
+/** 12.智能指针 */
+/* 定义一个帮助释放指针变量的类
+ */
+class P12 {
+public:
+    P12(P11 *p) {
+        this->p = p;
+    }
+    ~P12() {
+        delete this->p;
+    }
+    P11 * operator->() {
+        return p;
+    }
+
+    P11 *p;
+};
+void test12() {
+    P11 *p11 = new P11(10);
+    P12 p12 = P12(p11);
+    cout<< p12->num<< endl;
+}
+
+/** 13.重载等号(=) */
+class P13{
+public:
+    int age;
+    char *name;
+    P13(int age, char *name) {
+        this->age = age;
+        this->name = (char *)malloc(strlen(name) + 1);
+        strcpy(this->name, name);
+    }
+    P13 & operator=(P13 &p) {
+        this->age = age;
+        this->name = (char *)malloc(strlen(p.name) + 1);
+        strcpy(this->name, p.name);
+    }
+    void show() {
+        cout<< this->age<< " "<< this->name<< endl;
+    }
+};
+void test13() {
+    P13 p1(11, "123");
+    P13 p2 = p1;
+    p2.show();
+}
+
+/** 14.重载等于(==)和不等于(!=) */
+class P14{
+public:
+    int age;
+    string name;
+    P14(int age, string name) {
+        this->age = age;
+        this->name = name;
+    }
+    bool operator==(P14 &p) {
+        return this->age == p.age? true: false;
+    }
+};
+void test14() {
+    P14 p1(10, "lucy"), p2(12, "lilei");
+    if(p1 == p2) {
+        cout<< "p1 == p2"<< endl;
+    }else {
+        cout<< "p1 != p2"<< endl;
+    }
+}
 
 int main()
 {
@@ -190,5 +397,12 @@ int main()
     test4();
     test5();
     test6();
+    test8();
+    test9();
+    test10();
+    test11();
+    test12();
+    test13();
+    test14();
     return 0;
 }
